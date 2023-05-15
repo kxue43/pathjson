f8e7a77fa0beac1b3ed1341a0b6eea95770a3d23
Ke Xue <xueke.kent@gmail.com>
2023-05-15 16:40:23 -0400

    feat: Node classes that model JSON structure

---
+++ src/pathjson/_nodes.py
@@ -0,0 +1,133 @@
+# Builtin
+from abc import ABC, abstractmethod
+from functools import wraps
+from typing import (
+    Callable,
+    cast,
+    Dict,
+    List,
+    Optional,
+    Protocol,
+    TypeVar,
+    Union,
+)
+
+# Own
+from .exceptions import (
+    DuplicateNodeAdditionException,
+    MissingArrayIndexException,
+    NoneValuesAccessedException,
+)
+
+
+Scalar = Union[float, str, bool]
+Nodal = Union[Scalar, Dict[str, "Nodal"], List["Nodal"]]
+
+
+class CanGetItem(Protocol):
+    def __getitem__(self, key: str) -> Optional[Scalar]:
+        ...
+
+
+class Node(ABC):
+    jsonpath: str
+    _last_checked_row: Optional[CanGetItem]
+    _intersected_last_time: bool
+
+    def __init__(self, jsonpath: str) -> None:
+        self.jsonpath = jsonpath
+        self._last_checked_row = None
+        self._intersected_last_time = False
+
+    @abstractmethod
+    def get_value(self, row: CanGetItem) -> Nodal:
+        ...
+
+    @abstractmethod
+    def intersects(self, row: CanGetItem) -> bool:
+        ...
+
+
+S = TypeVar("S", bound=Node)
+
+
+def cached(
+    intersects: Callable[[S, CanGetItem], bool]
+) -> Callable[[S, CanGetItem], bool]:
+    @wraps(intersects)
+    def cached_intersects(self: S, row: CanGetItem) -> bool:
+        if self._last_checked_row is row:
+            return self._intersected_last_time
+        self._intersected_last_time = intersects(self, row)
+        self._last_checked_row = row
+        return self._intersected_last_time
+
+    return cached_intersects
+
+
+class LeafNode(Node):
+    def get_value(self, row: CanGetItem) -> Scalar:
+        if not self.intersects(row):
+            raise NoneValuesAccessedException(
+                f"Value at JSONPath `{self.jsonpath}` is `None`."
+            )
+        result = row[self.jsonpath]
+        return cast(Scalar, result)
+
+    @cached
+    def intersects(self, row: CanGetItem) -> bool:
+        return row[self.jsonpath] is not None
+
+
+class InternalNode(Node):
+    childrens: Dict[str, Node]
+
+    def __init__(self, jsonpath: str) -> None:
+        super().__init__(jsonpath)
+        self.childrens = {}
+
+    def add_child(self, key: str, child: Node) -> None:
+        if key in self.childrens:
+            raise DuplicateNodeAdditionException(
+                f"""
+                Child node `{child.jsonpath}` was added to parent node `{self.jsonpath}`
+                more than once during model-building.
+                """
+            )
+        self.childrens[key] = child
+
+    @cached
+    def intersects(self, row: CanGetItem) -> bool:
+        return any(map(lambda node: node.intersects(row), self.childrens.values()))
+
+
+class ObjectNode(InternalNode):
+    def get_value(self, row: CanGetItem) -> Dict[str, Nodal]:
+        if not self.intersects(row):
+            raise NoneValuesAccessedException(
+                f"Values at JSONPaths `{self.jsonpath}***` are all `None`."
+            )
+        return {
+            key: node.get_value(row)
+            for key, node in self.childrens.items()
+            if node.intersects(row)
+        }
+
+
+class ArrayNode(InternalNode):
+    def get_value(self, row: CanGetItem) -> List[Nodal]:
+        if not self.intersects(row):
+            raise NoneValuesAccessedException(
+                f"Values at JSONPaths `{self.jsonpath}***` are all `None`."
+            )
+        length = len(self.childrens)
+        for n in range(0, length):
+            if str(n) not in self.childrens:
+                raise MissingArrayIndexException(
+                    f"Missing a JSONPath of the format `{self.jsonpath}[{n}]***`."
+                )
+        list_: List[Nodal] = []
+        for n in range(0, length):
+            if self.childrens[str(n)].intersects(row):
+                list_.append(self.childrens[str(n)].get_value(row))
+        return list_
---
+++ src/pathjson/exceptions.py
@@ -0,0 +1,35 @@
+# Builtin
+from functools import wraps
+from textwrap import dedent
+
+
+class BaseException(Exception):
+    @classmethod
+    def __init_subclass__(cls, /, **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+        cls.__init__ = BaseException.__init_method_wrapper(cls.__init__)  # type: ignore
+
+    @staticmethod
+    def __init_method_wrapper(func, /):
+        @wraps(func)
+        def wrapped_init_method(self, /, *args):
+            func(self, *args)
+            if len(self.args) > 0 and isinstance(self.args[0], str):
+                one_line_exception_message = (
+                    dedent(self.args[0]).replace("\n", " ").strip()
+                )
+                self.args = (one_line_exception_message, *self.args[1:])
+
+        return wrapped_init_method
+
+
+class DuplicateNodeAdditionException(BaseException):
+    pass
+
+
+class MissingArrayIndexException(BaseException):
+    pass
+
+
+class NoneValuesAccessedException(BaseException):
+    pass
